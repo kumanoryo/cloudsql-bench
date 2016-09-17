937b2fd3cd5c30bb5083639b5ef912b2d17b945b
kumanoryo <r-kumano@groovenauts.jp>
2016-09-17 21:46:23 +0900

    Remove PK_VAR_packer_dir from packer template. The PACKER_DIR is different from cloud shell vm and docker container.

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -21,17 +21,6 @@ if [ "$(gcloud compute images list --format='table[no-heading](name)' --project=
   run gcloud compute images delete "${IMAGE_NAME}" --project="${PROJECT_ID}" --quiet || { echo_abort; exit 1; }
 fi
 
-#echo_info "# Set Env parameter."
-#run export PK_VAR_source_image="${source_image}" || { echo_abort; exit 1; }
-#run export PK_VAR_project_id="${IMAGE_PROJECT_ID}" || { echo_abort; exit 1; }
-#run export PK_VAR_network="${IMAGE_NETWORK}" || { echo_abort; exit 1; }
-#run export PK_VAR_zone="${IMAGE_ZONE}" || { echo_abort; exit 1; }
-#run export PK_VAR_ssh_username="${IMAGE_SSH_USERNAME}" || { echo_abort; exit 1; }
-#run export PK_VAR_image_name="${IMAGE_NAME}" || { echo_abort; exit 1; }
-#run export PK_VAR_cloudsql_instance_name="${IMAGE_CLOUDSQL_INSTANCE_NAME}" || { echo_abort; exit 1; }
-#run export PK_VAR_packer_dir="${PACKER_DIR}" || { echo_abort; exit 1; }
-#run export PK_VAR_bucket_name="${IMAGE_BUCKET_NAME}" || { echo_abort; exit 1; }
-
 echo_info "# Create image."
 packer_cmd="docker run -i -t \
 -v ${PACKER_DIR}/:/opt/ \
@@ -42,7 +31,6 @@ packer_cmd="docker run -i -t \
 -e PK_VAR_ssh_username=${IMAGE_SSH_USERNAME} \
 -e PK_VAR_image_name=${IMAGE_NAME} \
 -e PK_VAR_cloudsql_instance_name=${IMAGE_CLOUDSQL_INSTANCE_NAME} \
--e PK_VAR_packer_dir=${PACKER_DIR} \
 -e PK_VAR_bucket_name=${IMAGE_BUCKET_NAME} \
 hashicorp/packer:light \
 build /opt/packer-template.json"
