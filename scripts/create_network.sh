e2af9ed953718ad21d9e5d219d69573f9df09b36
kumanoryo <r-kumano@groovenauts.jp>
2016-09-17 23:46:35 +0900

    Add create network script and terraform template.

---
+++ scripts/create_network.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# shellcheck disable=SC1091
+
+set -u
+
+SCRIPTS_DIR=$(cd "$(dirname "$0")" && pwd)
+# shellcheck source=./initializing.sh
+. "${SCRIPTS_DIR}/function/initializing.sh"
+TERRAFORM_DIR="${HOME_DIR}/terraform/network"
+
+echo_begin_script
+
+echo_info "# Set remote tfstate file."
+terraform_cmd="docker run -i -t \
+-v ${TERRAFORM_DIR}/:/opt/ \
+-e TF_VAR_bucket_name=${BUCKET_NAME} \
+hashicorp/terraform:light \
+remote config \
+-backend=gcs \
+-backend-config=bucket=${BUCKET_NAME} \
+-backend-config=path=/opt/terraform-template/terraform.tfstate"
+
+echo_info "${terraform_cmd}"
+eval "${terraform_cmd}" || { echo_abort; exit 1; }
+
+echo_info "# Create network."
+terraform_cmd="docker run -i -t \
+-v ${TERRAFORM_DIR}/:/opt/ \
+-e TF_VAR_project_id=${PROJECT_ID} \
+-e TF_VAR_region=${REGION} \
+-e TF_VAR_bucket_name=${BUCKET_NAME} \
+-e TF_VAR_network_name=${NETWORK} \
+hashicorp/terraform:light \
+apply /opt/terraform-template/"
+
+echo_info "${terraform_cmd}"
+eval "${terraform_cmd}" || { echo_abort; exit 1; }
+
+echo_end_script
+
+exit 0
---
+++ terraform/network/terraform-template/terraform.tf
@@ -0,0 +1,31 @@
+variable "project_id" {}
+variable "region" {}
+variable "bucket_name" {}
+variable "network_name" {}
+
+provider "google" {
+  project     = "${var.project_id}"
+  region      = "${var.region}"
+}
+
+data "terraform_remote_state" "default" {
+    backend = "gcs"
+    config {
+        bucket = "${var.bucket_name}"
+        path = "/opt/network/terraform.tfstate"
+        project = "${var.project_id}"
+    }
+}
+
+resource "template_file" "default" {
+  template = "${greeting}"
+
+  vars {
+    greeting = "${data.terraform_remote_state.default.output.greeting}"
+  }
+}
+
+resource "google_compute_network" "default" {
+  name                    = "${var.network_name}"
+  auto_create_subnetworks = true
+}
